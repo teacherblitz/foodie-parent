cf7a3868f88fd385bf19d248f87dd4840fc3eec4
teacherblitz <teacherblitz@gmail.com>
2020-04-25 22:19:17 +0800

    程序分层初版

---
+++ foodie-api/src/main/java/com/teacherblitz/ApiApplication.java
@@ -0,0 +1,17 @@
+package com.teacherblitz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * api程序入口
+ * @author teacherblitz
+ * @since 2020/4/25
+ */
+@SpringBootApplication
+public class ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
